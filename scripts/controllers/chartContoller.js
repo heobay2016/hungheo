bf50c8b4269340cd272b19f4605fe99aef3f6737
Nguyen Phuc Hung <phuchung.nguyen78@gmail.com>
2016-02-12 14:41:33 +0700

    ddddd

--- scripts/controllers/chartContoller.js
+++ scripts/controllers/chartContoller.js
@@ -7,18 +7,48 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('ChartCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-    $scope.line = {
-	    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	    series: ['Series A', 'Series B'],
-	    data: [
-	      [65, 59, 80, 81, 56, 55, 40],
-	      [28, 48, 40, 19, 86, 27, 90]
-	    ],
-	    onClick: function (points, evt) {
-	      console.log(points, evt);
-	    }
-    };
+  .controller('ChartCtrl', ['$scope', '$timeout', '$http', '$interval', function ($scope, $timeout, $http, $interval) {
+
+	  $scope.sjcPrice = "http://www.sjc.com.vn/xml/tygiavang.xml";
+
+
+	  var startInterval = $interval(function(){
+		  console.warn('Get New Price...');
+
+		  $http({
+			  method: 'GET',
+			  url: $scope.sjcPrice
+		  }).then(function successCallback(response) {
+			  console.info('Get successfully!');
+
+			  console.log(response);
+
+		  }, function errorCallback(response) {
+
+
+		  }, function finallyCallback(response) {
+
+
+		  });
+	  }, 3000);
+
+
+	  $scope.line = {
+		  labels: ['T1', 'T2', 'March', 'April', 'May', 'June', 'July'],
+		  series: ['Series A', 'Series B'],
+		  data: [
+			  [65, 59, 80, 81, 56, 55, 40],
+			  [28, 48, 40, 19, 86, 27, 90]
+		  ],
+		  onClick: function (points, evt) {
+			  console.log(points, evt);
+		  }
+	  };
+
+
+
+
+
 
     $scope.bar = {
 	    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
